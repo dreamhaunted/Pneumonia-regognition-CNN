5c26fd90604b5324c10821ebdeff6f23b2318695
Vladislav Pronoza <dreamhauntedsky@gmail.com>
2020-05-26 11:26:05 +0300

    Updated train.py

--- train.py
+++ train.py
@@ -9,18 +9,20 @@ from tqdm import tqdm
 train, test, val = process_data()
 device = torch.device('cuda:0')
 net = Net().to(device)
-X = torch.Tensor([i[0] for i in train]).view(-1, 98, 98).to(device)
-# Normalize data
-X = X / 255.0
-y = torch.Tensor([i[1] for i in train]).to(device)
-
-optimizer = optim.Adam(net.parameters(), lr=0.001)
-loss_function = nn.MSELoss()
 
 def train_model(net):
+
+    X = torch.Tensor([i[0] for i in train]).view(-1, 98, 98).to(device)
+    # Normalize data
+    X = X / 255.0
+    y = torch.Tensor([i[1] for i in train]).to(device)
+
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    loss_function = nn.MSELoss()
+
     epochs = 2
     batch_size = 32
-    
+
     for epoch in tqdm(range(epochs)):
         running_loss = 0.0
         for i in range(0, len(X), batch_size):
@@ -33,7 +35,7 @@ def train_model(net):
             loss = loss_function(outputs, batch_y)
             loss.backward()
             optimizer.step()
-            
+
             running_loss += loss.item()
             if i % 512 == 0:
                 print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
@@ -46,7 +48,7 @@ def test_model(net):
     total = 0
     X = torch.Tensor([i[0] for i in test]).view(-1, 98, 98).to(device)
     y = torch.Tensor([i[1] for i in test]).to(device)
-    with torch.no_grad():    
+    with torch.no_grad():
         for i in tqdm(range(len(X))):
             real_target = torch.argmax(y[i])
             output = net(X[i].view(-1, 1, 98, 98))[0]
